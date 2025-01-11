06a8311fe4950656765593c2fd4b0a4c78eaa85c
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2025-01-12 04:59:31 +0530

    working on letting user select the model

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,31 @@
 use clap::Command;
-
-mod models;
-mod utils;
 use models::{anthropic::anthropic, gemini::gemini, openai::openai};
 use yansi::Paint;
 
-pub enum Models {
-    Openai,
-    Anthropic,
-    Gemini,
+mod models;
+mod utils;
+
+struct Model {
+    model: String,
 }
 
 fn main() {
     let description = "generate meaningful commit messages locally using AI. go to https://github.com/shivamhwp/git-acm for more details."
         .blue()
         .to_string();
-    let run_command = "explicit run command, does the same thing as running `git-acm`";
+    // let run_command = "explicit run command, does the same thing as running `git-acm`";
 
     let cli = Command::new("git-acm")
         .author("shivam [shivam.ing]")
         .version("0.1.2") // similar to cargo.toml file.
         .about(description)
-        .subcommand(Command::new("run").about(run_command))
         .subcommand(
             Command::new("use")
-                .override_help("choose from openai, anthropic or gemini")
                 .about("choose the api's you want to use ")
                 .subcommand(Command::new("openai"))
-                .about("runs openai api")
                 .subcommand(Command::new("anthropic"))
-                .about("runs anthropic api")
                 .subcommand(Command::new("gemini"))
-                .about("choose from openai, anthropic or gemini"),
+                .override_help("choose from openai, anthropic or gemini"),
         )
         .get_matches();
 
@@ -39,24 +33,8 @@ fn main() {
         Some(("run", _)) => {
             get_commit_msg();
         }
-        Some(("use", sub_matches)) => match sub_matches.subcommand() {
-            Some(("openai", _)) => {
-                println!("openai");
-                openai();
-            }
-            Some(("anthropic", _)) => {
-                println!("anthropic");
-                anthropic();
-            }
-            Some(("gemini", _)) => {
-                println!("gemini");
-                gemini();
-            }
-            _ => println!(
-                "No valid subcommand provided, available commands : openai , anthropic & gemini"
-            ),
-        },
-        None => {
+        Some(("use", sub_matches)) => {
+            println!("{:?}", sub_matches);
             get_commit_msg();
         }
         _ => {
@@ -66,5 +44,7 @@ fn main() {
 }
 
 fn get_commit_msg() {
-    gemini();
+    openai();
+    // anthropic();
+    // gemini();
 }
--- temp.md
+++ temp.md
@@ -7,18 +7,13 @@
 - [x] make it a cli tool.
 - [x] publish to cargo as a pacakge.
 - [ ] better error handling.
-- [ ] supports openai, anthropic as well.
-- [ ] github release.
+- [x] supports openai, anthropic as well. - 📍
+- [x] setup cli for diff apis
+- [x] github release.
 - [ ] publish to brew or binary release.
 
-maybe later -> a website for this project.
-
-problems with the code rn
-
-### v0.1.0 and v0.1.1
-
-solve the problems in the given order.
-
-- panic reading env's. - fixed
+letting users choose the models will be more benificial. (set a default and let users choos.)
 
 think from first principles and think where there can be a problem.
+
+users should have at least friction as possible.
