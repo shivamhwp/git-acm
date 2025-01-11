c35ffc84bccc685acb34a3f33b83b6d582d4d851
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2025-01-11 17:11:00 +0530

    refactor(main): simplify command structure

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,61 @@
 use clap::Command;
-use isahc::{prelude::*, Request};
-use serde_json::{json, Value};
-use std::env;
-use std::time::Duration;
+
+mod models;
+mod utils;
+use models::{anthropic::anthropic, gemini::gemini, openai::openai};
 use yansi::Paint;
 
-mod diff;
+pub enum Models {
+    Openai,
+    Anthropic,
+    Gemini,
+}
 
 fn main() {
+    let description = "generate meaningful commit messages locally using AI. go to https://github.com/shivamhwp/git-acm for more details."
+        .blue()
+        .to_string();
+    let run_command = "explicit run command, does the same thing as running `git-acm`";
+
     let cli = Command::new("git-acm")
         .author("shivam [shivam.ing]")
         .version("0.1.2") // similar to cargo.toml file.
-        .about("generate meaningful commit messages locally using AI")
-        .subcommand_required(false)
+        .about(description)
+        .subcommand(Command::new("run").about(run_command))
         .subcommand(
-            Command::new("run")
-                .about("explicit run command, does the same thing as running `git-acm` "),
+            Command::new("use")
+                .override_help("choose from openai, anthropic or gemini")
+                .about("choose the api's you want to use ")
+                .subcommand(Command::new("openai"))
+                .about("runs openai api")
+                .subcommand(Command::new("anthropic"))
+                .about("runs anthropic api")
+                .subcommand(Command::new("gemini"))
+                .about("choose from openai, anthropic or gemini"),
         )
-        // .override_help(help_message.to_string())
         .get_matches();
 
     match cli.subcommand() {
         Some(("run", _)) => {
             get_commit_msg();
         }
+        Some(("use", sub_matches)) => match sub_matches.subcommand() {
+            Some(("openai", _)) => {
+                println!("openai");
+                openai();
+            }
+            Some(("anthropic", _)) => {
+                println!("anthropic");
+                anthropic();
+            }
+            Some(("gemini", _)) => {
+                println!("gemini");
+                gemini();
+            }
+            _ => println!(
+                "No valid subcommand provided, available commands : openai , anthropic & gemini"
+            ),
+        },
         None => {
             get_commit_msg();
         }
@@ -34,60 +66,5 @@ fn main() {
 }
 
 fn get_commit_msg() {
-    //checks if env exists
-    dotenvy::dotenv().ok();
-    let api_key = env::var("GEMINI_API_KEY").expect("API_KEY must be set");
-    let api_url = env::var("GEMINI_API_URL").expect("API_URL must be set");
-
-    let prompt = include_str!("../assets/prompt.txt");
-
-    if prompt.is_empty() {
-        println!("{}", "no prompt found".red());
-        return;
-    }
-
-    let full_diff = diff::get_diff();
-
-    let uri = format!("{}?key={}", api_url, api_key);
-
-    let req_body = json!({
-    "tools": [{"code_execution": {}}],
-    "contents": [{
-    "parts":[
-    {
-    "text": format!("{:?}{}", prompt , full_diff)
-        }
-     ]
-          }] ,
-         });
-
-    let response = Request::post(uri)
-        .header("Content-Type", "application/json")
-        .timeout(Duration::from_secs(10))
-        .body(req_body.to_string())
-        .unwrap()
-        .send();
-
-    match response {
-        Ok(mut res) => match res.text() {
-            Ok(res) => {
-                // if you wanna more fields like promptTokenCount, totalTokenCount etc.
-                // println!("{}", v) > you'll get the field names > access them using serde_json's get method
-                // https://docs.rs/serde_json/latest/serde_json/enum.Value.html#method.get
-
-                let v: Value = serde_json::from_str(&res).unwrap();
-                let commit_msg = &v["candidates"][0]["content"]["parts"][0]["text"];
-
-                let final_msg = commit_msg.to_string();
-                let clear_msg = final_msg.trim_end();
-                println!("{}", clear_msg);
-            }
-            Err(e) => {
-                println!("{}", e.red())
-            }
-        },
-        Err(e) => {
-            println!("{}", e.red())
-        }
-    }
+    gemini();
 }
