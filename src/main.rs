92d140c2b3ae1175664cbb5d9136831e85592783
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2025-01-13 01:54:27 +0530

    ✅feat: Add dynamic model selection and saving

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 use clap::Command;
 use models::{anthropic::anthropic, gemini::gemini, openai::openai};
+use std::fs;
 use yansi::Paint;
 
 mod models;
 mod utils;
-
-struct Model {
-    model: String,
-}
 
 fn main() {
     let description = "generate meaningful commit messages locally using AI. go to https://github.com/shivamhwp/git-acm for more details."
@@ -34,7 +31,20 @@ fn main() {
             get_commit_msg();
         }
         Some(("use", sub_matches)) => {
-            println!("{:?}", sub_matches);
+            match sub_matches.subcommand() {
+                Some(("openai", _)) => {
+                    save_value("openai").expect("the default api was not set to openai")
+                }
+                Some(("anthropic", _)) => {
+                    save_value("anthropic").expect("the default api was not set to anthropic")
+                }
+                Some(("gemini", _)) => {
+                    save_value("gemini").expect("the default value was not set to gemini")
+                }
+                _ => {
+                    println!("{}", "choose an api to make requests".red())
+                }
+            }
             get_commit_msg();
         }
         _ => {
@@ -44,7 +54,39 @@ fn main() {
 }
 
 fn get_commit_msg() {
-    openai();
-    // anthropic();
-    // gemini();
+    let model = load_value();
+    match model.as_str() {
+        "openai" => openai(),
+        "anthropic" => anthropic(),
+        "gemini" => gemini(),
+        _ => {
+            println!("{}", "no default api found".red());
+            println!(
+                "{}",
+                "no api selected. choose from  [ openai | anthropic | gemini ]".red()
+            )
+        }
+    }
+}
+
+fn save_value(value: &str) -> std::io::Result<()> {
+    return fs::write("assets/model.txt", value);
+}
+
+fn load_value() -> String {
+    let model = "assets/model.txt";
+
+    match fs::read_to_string(model) {
+        Ok(s) => {
+            return s;
+        }
+        Err(e) => {
+            println!(
+                "{}",
+                "couldn't get the default model, running with gemini once".red()
+            );
+            println!("{}", e);
+            return "gemini".to_string();
+        }
+    }
 }
