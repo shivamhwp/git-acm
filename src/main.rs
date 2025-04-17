47b81c63c59db83ee46218c87cdf60f48fb6edcf
Shivam Sharma <91240327+shivamhwp@users.noreply.github.com>
2025-04-18 05:20:56 +0530

    i always forget to change versions

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn main() {
 
     let cli = Command::new("git-acm")
         .author("shivam [shivam.ing]")
-        .version("1.2.2") 
+        .version("1.2.3") 
         .about(description)
         .subcommand(
             Command::new("use")
